f60dd56a0b3b37ea5331d830ea726c5825f5fac1
mino1045: <minyoungoh1045@gmail.com:>
2025-03-31 17:50:57 +0900

    13113

--- realty/src/main/java/realty/property_dto.java
+++ realty/src/main/java/realty/property_dto.java
@@ -7,5 +7,7 @@ import lombok.Data;
 @Data
 @Repository("property_dto")
 public class property_dto {
+	int pidx,punit,pblocks;
+	String pname,paddress,ptype,psalsetype,psaledate,pmoveindate,pmig,pheating,pbuilder,pstatus,pdate;
 
 }
